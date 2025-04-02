df39da06c8073694e81e9c8ff20c795cc38cca32
hpeter19 <njoshpitar@gmail.com>
2025-04-02 19:16:48 +0300

    styling the first part of the search bar,the search bar

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+
+import './App.css'
+
+function App() {
+   return(
+    <div className='App'>
+       <div className='search-bar-container'>
+           <div>Search Bar</div>
+             <div>Search results</div>
+       </div>
+    </div>
+   );
+
+}
+
+export default App
