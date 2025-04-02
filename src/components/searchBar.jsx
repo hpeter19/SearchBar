ac636e891babcabc91119e54c9d3cbc7039d775c
hpeter19 <njoshpitar@gmail.com>
2025-04-02 20:26:55 +0300

    using usestate to get the current value of the search bar

--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React,{useState} from "react";
 import { FaSearch } from "react-icons/fa";
 import "./searchBar.css";
 
 export const SearchBar = () => {
+     const [input,setInput] =useState("");
+
   return (
     <div className="input-wrapper"> 
       <FaSearch id="search-icon" />
-      <input placeholder="search here..." />
+      <input placeholder="search here..." value={input}  onChange={(e) => setInput(e.target.value)}/>
     </div>
   );
 }
