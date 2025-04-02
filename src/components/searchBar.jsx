9613ccf02cc1528d476828bbdda812e57e22954d
hpeter19 <njoshpitar@gmail.com>
2025-04-02 20:42:37 +0300

    tracing the events

--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -5,10 +5,21 @@ import "./searchBar.css";
 export const SearchBar = () => {
      const [input,setInput] =useState("");
 
+     const fetchData = (value) => {
+        fetch("https://jsonplaceholder.typicode.com/users").
+        then((response) =>response.json.then())
+        .then((json)=> {
+            console.log(json);
+        });
+    }
+     const handleChange =(value) => {
+        setInput(value)
+        fetchData(value)
+     }
   return (
     <div className="input-wrapper"> 
       <FaSearch id="search-icon" />
-      <input placeholder="search here..." value={input}  onChange={(e) => setInput(e.target.value)}/>
+      <input placeholder="search here..." value={input}  onChange={(e) => handleChange(e.target.value)}/>
     </div>
   );
 }
