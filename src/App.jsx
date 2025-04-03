427161b31396a3db7562d1936ca0ce689dafa5f8
hpeter19 <njoshpitar@gmail.com>
2025-04-03 09:46:09 +0300

    creating the folder for storing lists

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import { useState } from 'react'
 
 import './App.css'
 import { SearchBar } from './components/searchBar';
+import { SearchResultsList } from './components/searchResultsList';
 
 function App() {
+   
+     const [results,setResults]= useState([])
+     
    return(
     <div className='App'>
        <div className='search-bar-container'>
-              <SearchBar />
-             <div>Search results</div>
+              <SearchBar  setResults={setResults}/>
+              <SearchResultsList />
        </div>
     </div>
    );
